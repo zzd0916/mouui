b41019090a0439e76cc98914b5f2846ad13bdd06
zhangzedong <983521365@qq.com>
2020-05-07 14:09:44 +0800

    add button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/index.scss';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Button>默认</Button>
+       <Button btnType={ButtonType.Danger}>危险</Button>
+       <Button size={ButtonSize.Large}>大小</Button>
+       <Button disabled={true} btnType={ButtonType.Link} href={"http://www.baidu.com"}>Link </Button>
+       <Button disabled={true} href={"http://www.baidu.com"}>disabled </Button>
       </header>
     </div>
   );
