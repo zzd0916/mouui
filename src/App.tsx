021c59fef5ef6d2808b00eff871fb4659ce1db13
zhangzedong <983521365@qq.com>
2020-05-07 23:16:18 +0800

    add 123

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/index.scss';
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
+  const a = 123;
   return (
     <div className="App">
       <header className="App-header">
